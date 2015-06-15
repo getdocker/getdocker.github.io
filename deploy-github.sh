b2da922ff0376af901c9df817d2985f1ed66a4d6
Samuel Chen <samuel.net@gmail.com>
2015-06-15 19:18:50 +0800

    .

---
+++ deploy-github.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIR=/tmp/getdocker_site
+
+
+git add .
+git commit -m '.'
+git push origin gh-pages
+
+rm -rf $DIR
+jekyll build -d /tmp/_site
+
+git checkout master
+cp -r ${DIR}/* .
+git add .
+git commit -m '.'
+git push origin master
+
+git checkout gh-pages
+
